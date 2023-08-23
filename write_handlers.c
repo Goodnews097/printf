43c3d6872a27e3630b946475b120f1ba4205fb9c
goodnews097 <uwanna.goodnews097@gmail.com>
2023-08-23 03:46:59 -0700

    RT

--- write_handlers.c
+++ write_handlers.c
@@ -22,55 +22,25 @@ int flags, int width, int precision, int size)
 int i = 0;
 
 char padd = ' ';
-
-
-
 UNUSED(precision);
-
 UNUSED(size);
-
-
-
 if (flags & F_ZERO)
-
 padd = '0';
-
-
-
 buffer[i++] = c;
-
 buffer[i] = '\0';
-
-
-
 if (width > 1)
-
 {
-
 buffer[BUFF_SIZE - 1] = '\0';
-
 for (i = 0; i < width - 1; i++)
-
 buffer[BUFF_SIZE - i - 2] = padd;
-
-
-
 if (flags & F_MINUS)
-
 return (write(1, &buffer[0], 1) +
-
 write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
-
 else
-
 return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-
 write(1, &buffer[0], 1));
-
 }
-
-
-
+{
 return (write(1, &buffer[0], 1));
 
 }
@@ -99,11 +69,7 @@ int length = BUFF_SIZE - ind - 1;
 
 char padd = ' ', extra_ch = 0;
 
-
-
 UNUSED(size);
-
-
 
 if ((flags & F_ZERO) && !(flags & F_MINUS))
 
@@ -120,9 +86,6 @@ extra_ch = '+';
 else if (flags & F_SPACE)
 
 extra_ch = ' ';
-
-
-
 return (write_num(ind, buffer, flags, width, precision,
 
 length, padd, extra_ch));
@@ -326,21 +289,13 @@ return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
 else /* Asign extra char to left of padding [padd>buffer]*/
 
 {
-
 return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
-
 }
 
 }
-
-
-
 return (write(1, &buffer[ind], length));
 
 }
-
-
-
 /**
  * write_pointer - Write a memory address
  * @buffer: Arrays of chars
@@ -356,15 +311,9 @@ return (write(1, &buffer[ind], length));
  */
 
 int write_pointer(char buffer[], int ind, int length,
-
 int width, int flags, char padd, char extra_c, int padd_start)
-
 {
-
 int i;
-
-
-
 if (width > length)
 
 {
@@ -436,5 +385,4 @@ if (extra_c)
 buffer[--ind] = extra_c;
 
 return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
-
 }
